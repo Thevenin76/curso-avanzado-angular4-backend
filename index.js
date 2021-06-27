cb35fe22af7cc818c77e7c8f56891efd0ee70663
David Yanes <david.yanes@correos.com>
2021-06-27 13:46:59 +0100

    Evitamos los warnings de mongoose.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 var mongoose = require ('mongoose');
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 mongoose.connect('mongodb://localhost:27017/zoo', (err, res) => {
      if (err){
