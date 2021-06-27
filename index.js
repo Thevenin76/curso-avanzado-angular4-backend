87c6df6220fc78411c4bb8f17323c57b3ee9538f
David Yanes <david.yanes@correos.com>
2021-06-27 13:59:08 +0100

    Avance.

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 var mongoose = require ('mongoose');
 
+mongoose.Promise = global.Promise;
+
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect('mongodb://localhost:27017/zoo', (err, res) => {
-     if (err){
-         throw err;
-     } else {
-         console.log ('LA conexión a la bbdd zoo se ha realizado correctamente.')
-     }
-});
+mongoose.connect('mongodb://localhost:27017/zoo')
+        .then( () => {            
+           console.log ('LA conexión a la bbdd zoo se ha realizado correctamente.')
+       })
+       .catch (err=>console.log('err'));
